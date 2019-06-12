87697621fe5e85141fb76f7cd2561a3b7b32a807
박경종 <kevinpark@webace.co.kr>
2019-06-12 18:07:11 +0900

    다중쿼리 실행 가능 하도록 업그레이드

--- rester-sql/resterSQL.class.php
+++ rester-sql/resterSQL.class.php
@@ -143,32 +143,39 @@ class resterSQL extends rester
     public function execute_sql($path)
     {
         $pdo = db::get($this->cfg->database());
-        $query = file_get_contents($path);
-
-        // 필터링 된 파라미터를 받아옴
-        // 영문숫자_-로 조합된 키워드 추출
-        $params = [];
-        preg_match_all('/:[a-zA-z0-9_-]+/', $query, $matches);
-        $matches = $matches[0];
-
-        foreach($matches as $bind_param)
-        {
-            foreach ($this->verify->param() as $k=>$v)
-            {
-                if(strpos($k,':')!==0) $k = ':'.$k;
-                if($bind_param==$k) $params[$bind_param] = $v;
-            }
-            if(!isset($params[$bind_param]))
-                throw new Exception("There is no parameter for bind. [{$bind_param}]");
-        }
-
+        $queries = file_get_contents($path);
         $response_data = [];
-        $stmt = $pdo->prepare($query,[PDO::ATTR_CURSOR, PDO::CURSOR_FWDONLY]);
-        $stmt->execute($params);
-        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        foreach($res as $row)
+        foreach(explode(';',$queries) as $query)
         {
-            $response_data[] = $row;
+            $stmt = $pdo->prepare($query,[PDO::ATTR_CURSOR, PDO::CURSOR_FWDONLY]);
+
+            // 필터링 된 파라미터를 받아옴
+            // 영문숫자_-로 조합된 키워드 추출
+            $params = [];
+            preg_match_all('/:[a-zA-z0-9_-]+/', $query, $matches);
+            $matches = $matches[0];
+
+            foreach($matches as $bind_param)
+            {
+                foreach ($this->verify->param() as $k=>$v)
+                {
+                    if(strpos($k,':')!==0) $k = ':'.$k;
+                    if($bind_param==$k)
+                    {
+                        $params[$bind_param] = $v;
+                    }
+                }
+                if(!isset($params[$bind_param]))
+                    throw new Exception("There is no parameter for bind. [{$bind_param}]");
+            }
+
+            $stmt->execute($params);
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach($res as $row)
+            {
+                $response_data[] = $row;
+            }
+            
         }
 
         return $response_data;
